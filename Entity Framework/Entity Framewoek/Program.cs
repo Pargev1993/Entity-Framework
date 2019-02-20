8e33a2ff61273d7384988cf6f9b7946e81f02396
pqoalla <pqoalla@gmail.com>
2019-02-20 12:28:24 -0800

    Model First

---
+++ Entity Framework/Entity Framewoek/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framewoek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+           using (Model1Container container = new  Model1Container())
+            {
+                    
+            }
+        }
+    }
+}
